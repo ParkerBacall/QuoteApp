e228150b376818b467a37edc8c52150ea430df88
KellyRudnicki <kelly.rudnicki@comcast.net>
2019-12-30 16:28:40 -0700

    added migration and seed to backend

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Quote.create(text: "Computer Science is no more about computers than astronomy is about telescopes.",
+author: "Edsger W. Dijkstra")
+
+Quote.create(text: "Simplicity is prerequisite for reliability.",
+"author": "Edsger W. Dijkstra")
+
+Quote.create(text: "The computing scientist’s main challenge is not to get confused by the complexities of his own making.",
+"author": "Edsger W. Dijkstra")
+
+Quote.create(text:"If debugging is the process of removing software bugs, then programming must be the process of putting them in.",
+"author": "Edsger W. Dijkstra")
