6d2d2c9d38bc28f3e544fce8d03ef64b3c0c1e64
ParkerBacall <pbacall2@gmail.com>
2019-12-31 12:57:09 -0700

    create functionality for tags

--- backend/app/controllers/tags_controller.rb
+++ backend/app/controllers/tags_controller.rb
@@ -8,4 +8,10 @@ class TagsController < ApplicationController
         tag = Tag.find(params[:id])
         render json: tag, include: :quotes
     end
+
+    def create
+        tag = Tag.create(name: params[:name])
+        render json: tag
+    end
+
 end
